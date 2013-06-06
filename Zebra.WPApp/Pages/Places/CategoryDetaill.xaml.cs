080618cb6c11573e8906cd5db118fb583a19bfae
Daniel Mauricio Pedernera Parada <daniel7pp@hotmail.es>
2013-06-06 11:21:14 -0400

    cambios dani

--- Zebra.WPApp/Pages/Places/CategoryDetaill.xaml.cs
+++ Zebra.WPApp/Pages/Places/CategoryDetaill.xaml.cs
@@ -1,27 +1,40 @@
-﻿using Microsoft.Phone.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 using ZebrasLib.Classes;
 
 namespace Zebra.WPApp.Pages.Places
 {
-    public partial class CategoriesPage : PhoneApplicationPage
+    public partial class CategoryDetaill : PhoneApplicationPage
     {
-        public CategoriesPage()
+        public CategoryDetaill()
         {
             InitializeComponent();
             this.Loaded += CategoriesPage_Loaded;
         }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+             base.OnNavigatedTo(e);
+             string cat = NavigationContext.QueryString["category"];
+             txtCambiar.Title = cat;
+        }
+        
 
         private void CategoriesPage_Loaded(object sender, RoutedEventArgs e)
         {
             List<Category> listCategories = new List<Category>();
-            listCategories.Add(new Category { name = "Coffe" });
+            listCategories.Add(new Category { name = "Coffe"  });
             listCategories.Add(new Category { name = "Coffe1" });
             listCategories.Add(new Category { name = "Coffe2" });
             listCategories.Add(new Category { name = "Coffe3" });
             listCategories.Add(new Category { name = "Coffe4" });
+            listCategories.Add(new Category { name = "Coffe5" });
 
             lstCategories.ItemsSource = listCategories;
         }
